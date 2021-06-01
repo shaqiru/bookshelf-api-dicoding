722e31b6f09c9b62a8a8085d73bac839223dcada
Kreideprinz <jbr.abdul@upi.edu>
2021-06-01 15:04:23 +0700

    commit pertama

---
+++ src/routes.js
@@ -0,0 +1,36 @@
+const {
+  addBook,
+  getAllBooks,
+  getBookById,
+  editBookById,
+  deleteBookById
+} = require('./handler')
+
+const routes = [{
+  method: 'POST',
+  path: '/books',
+  handler: addBook
+},
+{
+  method: 'GET',
+  path: '/books/{id}',
+  handler: getBookById
+},
+{
+  method: 'GET',
+  path: '/books',
+  handler: getAllBooks
+},
+{
+  method: 'PUT',
+  path: '/books/{id}',
+  handler: editBookById
+},
+{
+  method: 'DELETE',
+  path: '/books/{id}',
+  handler: deleteBookById
+}
+]
+
+module.exports = routes
